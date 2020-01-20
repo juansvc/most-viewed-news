522ca6f86ae81644be49f1923b206bdd755f34e6
juansvc <juansebastianvc@gmail.com>
2020-01-19 22:35:47 -0500

    Diseño inicial widget, uso fuentes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div></div>
-  );
+class App extends Component {
+
+  render() {
+    return (
+      <div className="container">
+        <h1>LO MÁS LEÍDO</h1>
+                <div className="articulo">
+                  <div className="articulo-numero">
+                    #
+                  </div>
+                  <div className="articulo-body">
+                    <h4 className="articulo-titulo">
+                      Prueba Noticia
+                    </h4>
+                  </div>
+                </div>
+            
+      </div>
+    )
+}
 }
 
 export default App;
