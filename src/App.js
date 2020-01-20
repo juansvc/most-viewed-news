ad4f267984c51b43602a253149b54ec7fb323d73
juansvc <juansebastianvc@gmail.com>
2020-01-19 22:50:36 -0500

    Llamado al API, uso del API por # visitas, límite  5 noticias, artículos, título con link, numeración

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import "./App.css";
 class App extends Component {
 
   state = {
+    // Datos API eluniverso
+    todos: [],
+    // Numerador
+    i: 0,
     // Secciones
     secciones: [
       { value: "", display: "Todas" },
@@ -16,6 +20,22 @@ class App extends Component {
     seccionActual: ""
   };
 
+
+  componentDidMount() {
+
+    // Llamado API eluniverso
+    fetch(
+      "http://api.chartbeat.com/live/toppages/v3/?apikey=489ca5d5c6e07f057ec7d9a6a69be9d8&host=eluniverso.com"
+    )
+      .then(res => res.json())
+      .then(data => {
+        this.setState({ todos: data.pages });
+        console.log(this.state.todos);
+      })
+      .catch(console.log);
+  }
+
+
   render() {
     return (
       <div className="container">
@@ -26,7 +46,9 @@ class App extends Component {
           value={this.state.seccionActual}
           onChange={e =>
             this.setState({
-              seccionActual: e.target.value
+              seccionActual: e.target.value,
+              // Reseteo Numerador al cambio de sección
+              i: 0
             })
           }
         >
@@ -40,16 +62,34 @@ class App extends Component {
         {/* Titulo */}
         <h1>LO MÁS LEÍDO</h1>
         {/* Noticia */}
+        {/* Recorrido datos por número de visitas */}
+        {this.state.todos
+          .sort((a, b) => b.stats.visits - a.stats.visits)
+          .map(todo => {
+            {/* Sección Todas, article diferente a 0, mostrar 5 */}
+            if (
+              this.state.seccionActual === "" &&
+              todo.stats.article != 0 &&
+              this.state.i < 5
+            ) {
+              return (
                 <div className="articulo">
                   <div className="articulo-numero">
-                    #
+                  {/* Numeración */}
+                    {(this.state.i = this.state.i + 1)}
                   </div>
                   <div className="articulo-body">
                     <h4 className="articulo-titulo">
-                      Prueba Noticia
+                    {/* Título con link */}
+                      <a href={"https://" + todo.path} target="_blank">
+                        {todo.title}
+                      </a>
                     </h4>
                   </div>
                 </div>
+              );
+            }
+          })}
             
       </div>
     )
