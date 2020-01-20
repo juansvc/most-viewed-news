08196e9ea79c60752fdcfb6650506ea814fbeb5d
juansvc <juansebastianvc@gmail.com>
2020-01-19 22:40:50 -0500

    Lista desplegable (noticias, guayaquil, deportes, entretenimiento, la revista.)

--- src/App.js
+++ src/App.js
@@ -3,10 +3,43 @@ import "./App.css";
 
 class App extends Component {
 
+  state = {
+    // Secciones
+    secciones: [
+      { value: "", display: "Todas" },
+      { value: "noticias", display: "Noticias" },
+      { value: "guayaquil", display: "Guayaquil" },
+      { value: "deportes", display: "Deportes" },
+      { value: "entretenimiento", display: "Entretenimiento" },
+      { value: "la revista", display: "La Revista" }
+    ],
+    seccionActual: ""
+  };
+
   render() {
     return (
       <div className="container">
+      <h3>Seleccione una Sección:</h3>
+      {/* Lista Desplegable Secciones */}
+        <div className="selectSeccion">
+        <select
+          value={this.state.seccionActual}
+          onChange={e =>
+            this.setState({
+              seccionActual: e.target.value
+            })
+          }
+        >
+          {this.state.secciones.map(seccion => (
+            <option key={seccion.value} value={seccion.value}>
+              {seccion.display}
+            </option>
+          ))}
+        </select>
+        </div>
+        {/* Titulo */}
         <h1>LO MÁS LEÍDO</h1>
+        {/* Noticia */}
                 <div className="articulo">
                   <div className="articulo-numero">
                     #
